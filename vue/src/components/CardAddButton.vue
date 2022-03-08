33abc80b2dafb1130a8c08f74b0ac151c34f0f26
Leo <lcheung@swellbottle.com>
2022-03-08 08:14:02 -0500

    link addcard mutation to addcard button

--- vue/src/components/CardAddButton.vue
+++ vue/src/components/CardAddButton.vue
@@ -1,11 +1,28 @@
 <template>
   <div
+    @click="addCard"
     class="rounded-sm p-2 text-gray-600 cursor-pointer hover:bg-gray-400 hover:text-gray-800 text-sm"
   >
     Add new card
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import CreateCard from '../gql/mutations/CreateCard.gql'
+import { useMutation } from '@vue/apollo-composable'
+
+const { mutate: createCard } = useMutation(CreateCard, {
+  variables: {
+    listId: 1,
+    title: 'Test from code',
+    order: 1,
+    owner_id: 1,
+  },
+})
+
+function addCard() {
+  createCard()
+}
+</script>
 
 <style scoped></style>

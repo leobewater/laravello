da63a3c0040b3c617005202d2f169924b5141dab
Leo <lcheung@swellbottle.com>
2022-03-08 15:54:42 -0500

    comment out input field focus

--- vue/src/components/CardEditor.vue
+++ vue/src/components/CardEditor.vue
@@ -3,7 +3,7 @@
     <textarea
       class="rounded-md shadow-card py-1 px-2 outline-none w-full text-gray-900 text-sm bg-white h-16 resize-none"
       placeholder="Enter a title for this card..."
-      v-model="title"
+      v-model.trim="title"
       @keyup.esc="closed"
       @keyup.enter="addCard"
     ></textarea>
@@ -25,22 +25,23 @@
 </template>
 
 <script setup>
-import { onMounted, ref, defineEmits } from 'vue'
-import CreateCard from '../gql/mutations/CreateCard.gql'
-import BoardQuery from '../gql/queries/BoardWithListsAndCards.gql'
-import { useMutation } from '@vue/apollo-composable'
-import produce from 'immer'
+import { onMounted, ref, defineEmits } from "vue";
+import CreateCard from "../gql/mutations/CreateCard.gql";
+import BoardQuery from "../gql/queries/BoardWithListsAndCards.gql";
+import { useMutation } from "@vue/apollo-composable";
+import produce from "immer";
 
-const emit = defineEmits(['closed'])
+const emit = defineEmits(["closed"]);
 const props = defineProps({
   list: Object,
-})
+});
 
-const title = ref('')
+const title = ref("");
 
 onMounted(() => {
-  //  title.value.focus()
-})
+  // adding ref="title" to the textarea doesn't work with v-model.
+  // title.value.focus();
+});
 
 const { mutate: createCard } = useMutation(CreateCard, () => ({
   variables: {
@@ -57,14 +58,14 @@ const { mutate: createCard } = useMutation(CreateCard, () => ({
   update: (cache, { data: { createCard } }) => {
     // console.log(title.value)
     // console.log(props.list.id)
-    console.log(props.list.board_id)
+    // console.log(props.list.board_id)
 
     // read the cached query
     const data = cache.readQuery({
       query: BoardQuery,
       variables: { id: parseInt(props.list.board_id) },
-    })
-    console.log(createCard)
+    });
+    console.log(createCard);
 
     // console.log(data)
     cache.writeQuery({
@@ -74,18 +75,18 @@ const { mutate: createCard } = useMutation(CreateCard, () => ({
         // push new card to the list
         x.board.lists
           .find((itemList) => itemList.id === props.list.id)
-          .cards.push(createCard)
+          .cards.push(createCard);
       }),
-    })
+    });
   },
-}))
+}));
 
 function addCard() {
-  createCard()
-  closed()
+  createCard();
+  closed();
 }
 
 function closed() {
-  emit('closed')
+  emit("closed");
 }
 </script>

db5e7ede136b49f34191b4ffc739d04a5d4c404e
Leo <lcheung@swellbottle.com>
2022-03-08 07:10:54 -0500

    install apollo-composable

--- vue/tailwind.config.js
+++ vue/tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       keyframes: {

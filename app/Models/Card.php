1c08ce10a1c209abe69fbb417b984d44b8984358
Leo <lcheung@swellbottle.com>
2022-03-07 20:22:41 -0500

    set up card, cardlist, board, user relationship

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Card extends Model
 {
     use HasFactory;
+
+    // list relationship
+    public function list(): BelongsTo
+    {
+        return $this->belongsTo(CardList::class, 'list_id');
+    }
+
+    // owner relationship
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -41,4 +42,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // board relationship
+    public function boards(): HasMany
+    {
+        return $this->hasMany(Board::class, 'board_id');
+    }
+
+    // cards relationship
+    public function cards(): HasMany
+    {
+        return $this->hasMany(Card::class, 'owner_id');
+    }
 }

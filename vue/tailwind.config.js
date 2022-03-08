e9063188b04fa41e630a62b2239dd33ddcb7d943
Leo <lcheung@swellbottle.com>
2022-03-08 12:58:31 -0500

    extend custom shadow

--- vue/tailwind.config.js
+++ vue/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      boxShadow: {
+        card: '0 1px 0 rgba(9, 30, 66, 0.25)',
+      },
       keyframes: {
         'fade-in-down': {
           from: {

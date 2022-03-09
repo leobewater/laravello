5d834e92f89d911cb093754439298d2465772f78
Leo <lcheung@swellbottle.com>
2022-03-09 09:33:09 -0500

    add login and route

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,13 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Board from '../views/Board.vue'
+import Login from '../views/Login.vue'
 import store from '../store'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Board',
-    component: Board,
-  },
+  { path: '/', name: 'Board', component: Board },
+  { path: '/login', name: 'Login', component: Login },
 ]
 
 const router = createRouter({
@@ -16,15 +14,15 @@ const router = createRouter({
 })
 
 //router.beforeEach((to, from, next) => {
-  // check user token when "requiresAuth" exists in "meta" key
-  // if (to.meta.requiresAuth && !store.state.user.token) {
-  //   next({ name: 'Login' })
-  // } else if (store.state.user.token && to.meta.isGuest) {
-  //   // redirect to dashboard if user already logged in and try to go to login or register pages
-  //   next({ name: 'Board' })
-  // } else {
-  //   next()
-  // }
+// check user token when "requiresAuth" exists in "meta" key
+// if (to.meta.requiresAuth && !store.state.user.token) {
+//   next({ name: 'Login' })
+// } else if (store.state.user.token && to.meta.isGuest) {
+//   // redirect to dashboard if user already logged in and try to go to login or register pages
+//   next({ name: 'Board' })
+// } else {
+//   next()
+// }
 //})
 
 export default router

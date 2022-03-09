320007d5ed31cf091eec089ecee0734c9905a96d
Leo <lcheung@swellbottle.com>
2022-03-09 10:39:46 -0500

    add middleware to lighthouse for sanctum

--- config/lighthouse.php
+++ config/lighthouse.php
@@ -28,6 +28,9 @@ return [
          * make sure to return spec-compliant responses in case an error is thrown.
          */
         'middleware' => [
+            // for laravel Sanctum
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+
             \Nuwave\Lighthouse\Support\Http\Middleware\AcceptJson::class,
 
             // Logs in a user if they are authenticated. In contrast to Laravel's 'auth'

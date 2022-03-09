93b15f78985aa7e20cc7845f8b6c745315d95191
Leo <lcheung@swellbottle.com>
2022-03-09 09:03:50 -0500

    add editing condition to card and show card editor

--- vue/src/components/Card.vue
+++ vue/src/components/Card.vue
@@ -1,5 +1,6 @@
 <template>
   <div
+    v-if="!editing"
     class="group bg-white shadow-card rounded-sm p-2 cursor-pointer text-sm hover:bg-gray-100 mb-2 flex justify-between"
   >
     <div>{{ props.card.title }}</div>
@@ -7,23 +8,33 @@
     <div
       class="flex font-bold opacity-0 group-hover:opacity-100 transition-opacity ease-out duration-500"
     >
-      <div class="text-gray-400 pr-2 hover:text-yellow-700">e</div>
+      <div
+        class="text-gray-400 pr-2 hover:text-yellow-700"
+        @click="editing = true"
+      >
+        e
+      </div>
       <div class="text-gray-400 hover:text-yellow-700" @click="cardDelete">
         d
       </div>
     </div>
   </div>
+  <CardEditor v-else class="mb-2" label="Save Card" />
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { useMutation } from '@vue/apollo-composable'
 import DeleteCard from '../gql/mutations/DeleteCard.gql'
 import { EVENT_CARD_DELETED } from '../constants.js'
+import CardEditor from './CardEditor.vue'
 
 const emit = defineEmits(['deleted'])
 const props = defineProps({
   card: Object,
 })
+
+const editing = ref(false)
 
 const { mutate: deleteCard } = useMutation(DeleteCard, () => ({
   variables: {
